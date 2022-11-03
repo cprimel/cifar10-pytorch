b271036900efbddca7db294ad8b1dbfa1a887a3c
Casey Primel <casey.primel@gmail.com>
2022-11-03 17:04:40 -0400

    added ConvMixer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from torchsummary import summary
 
-from models.resnet import ResNeXt10_32_2d
+from models.convmixer import ConvMixer
 
 if __name__ == "__main__":
-    net = ResNeXt10_32_2d()
-    summary(net, (3,32,32), device="cpu")
+    net = ConvMixer()
+    summary(net, (3, 32, 32), device="cpu")
---
+++ models/convmixer.py
@@ -0,0 +1,34 @@
+from typing import Callable
+
+import torch.nn as nn
+from torch import Tensor
+
+
+class Residual(nn.Module):
+    def __init__(self, fn: Callable):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.fn(x) + x
+
+
+def ConvMixer(dim: int = 256, depth: int = 8, kernel_size: int = 5, patch_size: int = 2,
+              n_classes: int = 10) -> nn.Sequential:
+    return nn.Sequential(
+        nn.Conv2d(3, dim, kernel_size=patch_size, stride=patch_size),
+        nn.GELU(),
+        nn.BatchNorm2d(dim),
+        *[nn.Sequential(Residual(nn.Sequential(
+            nn.Conv2d(dim, dim, kernel_size, groups=dim, padding="same"),
+            nn.GELU(),
+            nn.BatchNorm2d(dim)
+        )),
+            nn.Conv2d(dim, dim, kernel_size=1),
+            nn.GELU(),
+            nn.BatchNorm2d(dim)
+        ) for _ in range(depth)],
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Linear(dim, n_classes)
+    )
