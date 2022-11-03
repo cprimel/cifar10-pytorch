dd72e13a63bf791aba36838ad88982be11a0db62
Casey Primel <casey.primel@gmail.com>
2022-11-03 16:28:45 -0400

    modified ResNet class to enable ResNeXt arch and added ResNext10_32_2d to models

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from torchsummary import summary
 
-from models.resnet import ResNet10
+from models.resnet import ResNeXt10_32_2d
 
 if __name__ == "__main__":
-    net = ResNet10()
+    net = ResNeXt10_32_2d()
     summary(net, (3,32,32), device="cpu")
--- models/resnet.py
+++ models/resnet.py
@@ -17,7 +17,8 @@ def conv1x1(in_channels: int, out_channels: int, stride: int = 1) -> nn.Conv2d:
 class ResBlock(nn.Module):
     expansion: int = 1
 
-    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, padding: int = 1):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, groups: int = 1, base_width: int = 64,
+                 padding: int = 1):
         super().__init__()
         self.conv1 = conv3x3(in_channels, out_channels, stride)
         self.bn1 = nn.BatchNorm2d(num_features=out_channels)
@@ -61,8 +62,8 @@ class ResBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion: int = 4
 
-    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, padding: int = 1, base_width: int = 64,
-                 groups: int = 1):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, base_width: int = 64, groups: int = 1,
+                 padding: int = 1):
         super().__init__()
         width = int(out_channels * base_width / 64.0) * groups
         self.conv1 = conv1x1(in_channels, width)
@@ -110,14 +111,17 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     """
         TODO: Weight initialization
-        TODO: Dilation - not necessary but might be useful for iterating over different architectures
+        TODO: Dilation - not necessary but might be useful for iterating over different architectures?
     """
 
-    def __init__(self, block: Type[Union[ResBlock, Bottleneck]], layers: List[int], num_classes: int = 10):
+    def __init__(self, block: Type[Union[ResBlock, Bottleneck]], layers: List[int], num_classes: int = 10,
+                 groups: int = 1, width_per_group: int = 64):
         super().__init__()
         assert len(layers) == 4, f"layers requires a list of 4 integers, got {len(layers)}"
 
         self.in_channels = 64
+        self.groups = groups
+        self.base_width = width_per_group
         self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(self.in_channels)
         self.relu = nn.ReLU(inplace=True)
@@ -133,10 +137,10 @@ class ResNet(nn.Module):
     def _make_layer(self, block: Type[Union[ResBlock, Bottleneck]], out_channels: int, blocks: int,
                     stride: int = 1) -> nn.Sequential:
         layers = []
-        layers.append(block(self.in_channels, out_channels, stride))
+        layers.append(block(self.in_channels, out_channels, stride, self.base_width, self.groups))
         self.in_channels = out_channels * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.in_channels, out_channels))
+            layers.append(block(self.in_channels, out_channels, stride, base_width=self.base_width, groups=self.groups))
         return nn.Sequential(*layers)
 
     def forward(self, x: Tensor) -> Tensor:
@@ -162,3 +166,7 @@ def ResNet18():
 
 def ResNet10():
     return ResNet(ResBlock, [1, 1, 1, 1])
+
+
+def ResNeXt10_32_2d():
+    return ResNet(Bottleneck, [1, 1, 1, 1], groups=32, width_per_group=2)
