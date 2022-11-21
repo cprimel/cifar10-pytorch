1b3380f30b499cb2696b8c50f7ac9d11b9b3ae97
Casey Primel <casey.primel@gmail.com>
2022-11-21 11:05:49 -0500

    added resnet18 to model registry

--- models/__init__.py
+++ models/__init__.py
@@ -7,13 +7,14 @@ where the model is defined.
 
 from .convmixer import ConvMixer256_8_k5_p1, ConvMixer256_8_k9_p1, ConvMixer256_16_k9_p2, ConvMixer256_8_k5_p2, \
     ConvMixer256_8_k5_p1, ConvMixer256_8_k9_p2
-from .resnet import ResNet10, ResNeXt10_32_2d
+from .resnet import ResNet10, ResNet18, ResNeXt10_32_2d
 from .resnet_s import ResNetS20, ResNetS38
 
 model_registry = {
     "resnet_s20": ResNetS20,
     "resnet_s38": ResNetS38,
     "resnet10": ResNet10,
+    "resnet18": ResNet18,
     "resnext10_32_2d": ResNeXt10_32_2d,
     "convmixer256_8_k5_p1": ConvMixer256_8_k5_p1,
     "convmixer256_8_k5_p2": ConvMixer256_8_k5_p2,
